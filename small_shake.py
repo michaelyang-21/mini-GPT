a08dbb918db749b352b9ec0031900d8ac629fd4a
qiren <mic14cal14@gmail.com>
2023-05-25 00:46:15 -0400

    larger training

--- small_shake.py
+++ small_shake.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 512 # what is the maximum context length for predictions?
 max_iters = 10000
 eval_interval = 100
-learning_rate = 1e-3
+learning_rate = 1e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
+eval_iters = 100
 n_embd = 64
 n_head = 4
 n_layer = 4
