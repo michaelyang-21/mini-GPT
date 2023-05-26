df450d91db387ddbb0af5382ee3ab2aded6bd20f
qiren <mic14cal14@gmail.com>
2023-05-25 21:56:23 -0400

    input

--- small_shake.py
+++ small_shake.py
@@ -21,7 +21,7 @@ print('device:', device)
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('three.txt', 'r', encoding='utf-8') as f:
+with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
